f9b0c2b1149c7714bbce63703dfb237ebcecf761
Alan Ferreira <alanjjr83@gmail.com>
2021-01-25 20:31:28 -0300

    Ultimo commit da Aula 01 Imersao React V2

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <p></p>
           </Widget.Content>
         </Widget>
         <Footer />
