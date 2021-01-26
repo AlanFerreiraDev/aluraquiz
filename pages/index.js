bb93cf9ff11b1c221a5fae0514a9066ef5807ea9
Alan Ferreira <alanjjr83@gmail.com>
2021-01-26 10:54:23 -0300

    Primeiro Commit Aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import styled from 'styled-components'
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';// Esse import é direto do node_modules, não criamos nenhum cmponent para isso
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+
+// <link rel="preconnect" href="https://fonts.gstatic.com">
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -27,6 +32,9 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz Alura Planeta Terra</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -42,7 +50,7 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p></p>
+            <p />
           </Widget.Content>
         </Widget>
         <Footer />
